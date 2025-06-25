6714f3fb1074f4f7dcd8e0eecb0101b2d23bc4c9
Zenzen <thamzien@gmail.com>
2025-06-25 11:30:28 +1000

    Add source code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   basePath: '/my_portfolio',
+  assetPrefix: '/my_portfolio/',
   trailingSlash: true,
 };
 
